5c8bffbf9f3e7d2f5e2429970bc8b217ecddf3fe
Brayan Ricaldi <brayan.r.cool@gmail.com>
2023-09-19 15:52:00 -0300

    Agrego test de coordinate

--- src/coordinate.rs
+++ src/coordinate.rs
@@ -10,7 +10,7 @@ impl Coordinate {
     }
 
     pub fn down(&self) -> Option<Coordinate> {
-        if self.x == self.max_value {
+        if self.x + 1 == self.max_value {
             return None;
         }
         Some(Coordinate::new(self.x + 1, self.y, self.max_value))
@@ -24,7 +24,7 @@ impl Coordinate {
     }
 
     pub fn right(&self) -> Option<Coordinate> {
-        if self.y == self.max_value {
+        if self.y + 1 == self.max_value {
             return None;
         }
         Some(Coordinate::new(self.x, self.y + 1, self.max_value))
@@ -38,21 +38,63 @@ impl Coordinate {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn test_down_within_bounds() {
-//         let coordinate = Coordinate::new(2, 3, 4);
-//         let result = coordinate.down();
-//         assert_eq!(result, Some(Coordinate::new(3, 3, 4)));
-//     }
+    #[test]
+    fn test_down_within_bounds() {
+        let coordinate = Coordinate::new(2, 3, 4);
+        let result = coordinate.down();
+        assert_eq!(result, Some(Coordinate::new(3, 3, 4)));
+    }
 
-//     #[test]
-//     fn test_down_at_max_value() {
-//         let coordinate = Coordinate::new(4, 3, 4);
-//         let result = coordinate.down();
-//         assert_eq!(result, None);
-//     }
-// }
+    #[test]
+    fn test_down_at_max_value() {
+        let coordinate = Coordinate::new(3, 3, 4);
+        let result = coordinate.down();
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_up() {
+        let coordinate = Coordinate::new(2, 3, 5);
+        let result = coordinate.up();
+        assert_eq!(result, Some(Coordinate::new(1, 3, 5)));
+    }
+
+    #[test]
+    fn test_up_at_minimum_x() {
+        let coordinate = Coordinate::new(0, 3, 5);
+        let result = coordinate.up();
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_right() {
+        let coordinate = Coordinate::new(2, 3, 5);
+        let result = coordinate.right();
+        assert_eq!(result, Some(Coordinate::new(2, 4, 5)));
+    }
+
+    #[test]
+    fn test_right_at_maximum_y() {
+        let coordinate = Coordinate::new(2, 4, 5);
+        let result = coordinate.right();
+        assert_eq!(result, None);
+    }
+    #[test]
+    fn test_left() {
+        let coord = Coordinate::new(2, 3, 5);
+        let result = coord.left();
+        assert_eq!(result, Some(Coordinate::new(2, 2, 5)));
+    }
+
+    #[test]
+    fn test_left_at_minimum_y() {
+        let coord = Coordinate::new(2, 0, 5);
+        let result = coord.left();
+        assert_eq!(result, None);
+    }
+
+}
